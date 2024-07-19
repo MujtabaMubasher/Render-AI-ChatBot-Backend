f8931723e431eacd060f723d2ddfaf677d7d837b
Mujtaba Mubasher <81616586+MujtabaMubasher@users.noreply.github.com>
2024-07-19 08:09:26 +0500

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,18 @@
 import express from "express";
 import { config } from "dotenv";
-import morgan from "morgan"
+import morgan from "morgan";
 import router from "./routes/router.js";
 import cookieParser from "cookie-parser";
-import cors from 'cors'
+import cors from 'cors';
 
 config();
 const app = express();
 
-// use for Read some data from client side
-// Middlewares
+// Use for reading data from the client side
 app.use(express.json());
-app.use(cookieParser(process.env.COOKIE_SECRET))
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
-const allowedOrigins = ['https://mujtaba-gpt.vercel.app'];
+const allowedOrigins = ['https://mujtaba-gpt.vercel.app', 'http://localhost:5173'];
 
 const corsOptions = {
   origin: function (origin, callback) {
@@ -31,6 +30,15 @@ const corsOptions = {
 // Apply CORS middleware to all routes
 app.use(cors(corsOptions));
 
+// Explicitly handle preflight requests
+app.options('*', cors(corsOptions));
+
+// Middleware to log request headers for debugging
+app.use((req, res, next) => {
+  console.log('Request Headers:', req.headers);
+  next();
+});
+
 // Middleware to set CORS headers explicitly
 app.use((req, res, next) => {
   const origin = req.headers.origin;
@@ -43,55 +51,21 @@ app.use((req, res, next) => {
   next();
 });
 
-
-  // Middleware to clear unwanted cookies
-// app.use((req, res, next) => {
-//   res.clearCookie(".Tunnels.Relay.WebForwarding.Cookies", {
-//     path: "/",
-//     domain: "turbo-winner-9xg9g96xqp5fpr79-5137.app.github.dev",
-//     httpOnly: true,
-//     secure: true,
-//   });
-//   next();
-// });
-
-// remove it in production
-app.use(morgan("dev"));
+// Test route to verify CORS
+app.get('/api/v1/test-cors', (req, res) => {
+  res.json({ message: 'CORS is working' });
+});
 
 app.use("/api/v1", router);
 
-export default app
+// Error handling middleware
+app.use((err, req, res, next) => {
+  if (err) {
+    console.error('Error:', err.message);
+    res.status(500).send('CORS Error: ' + err.message);
+  } else {
+    next();
+  }
+});
 
-
-/*
-  Type of HTTP Request
-  1: GET 
-  // if We want to Get Data From backend or Database
-  2: PUT /
-  / If we want to modify, Mutate or Update Some Data
-  3: POST 
-  // If we want to send or Read some Data From Client. Such that Create Post
-  4: DELETE
-  // Delete Some Data
-*/
-
-/*
-app.get("/hello", (req,res,next)=>{
-      return res.send("Get Request");
-})
-
-app.post("/posts", (req,res,next)=>{
-  console.log(req.body.name);
-  return res.send("Post Request");
-})
-app.put("/put", (req,res,next)=>{
-  console.log(req.body.name);
-  return res.send("put Request");
-})
-
-app.delete("/user/:id", (req,res,next)=>{
-  console.log(req.params.id);
-  return res.send("delete Request");
-})
-
-*/
+export default app;
