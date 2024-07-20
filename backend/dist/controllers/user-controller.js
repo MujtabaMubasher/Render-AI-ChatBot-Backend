ba7ab5f6c003c5c1de38be8279f120984095b9f6
Mujtaba Mubasher <81616586+MujtabaMubasher@users.noreply.github.com>
2024-07-20 06:22:04 +0500

    Update user-controller.js

--- backend/dist/controllers/user-controller.js
+++ backend/dist/controllers/user-controller.js
@@ -74,8 +74,8 @@ const login = async (req, res) => {
             expires,
             httpOnly: true,
             signed: true,
-            secure: true,
-            sameSite: "Strict",
+            //secure: true,
+            //sameSite: "Strict",
         });
         const userLogin = await User.findById(userExist._id).select("-password");
         return res.status(200).json({
