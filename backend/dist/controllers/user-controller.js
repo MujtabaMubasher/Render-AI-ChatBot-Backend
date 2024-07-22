ff32d7eb8265ee54e59b7477134a6ab5260a9c18
Mujtaba Mubasher <81616586+MujtabaMubasher@users.noreply.github.com>
2024-07-22 22:37:05 +0500

    Update user-controller.js

--- backend/dist/controllers/user-controller.js
+++ backend/dist/controllers/user-controller.js
@@ -130,7 +130,7 @@ const logout = async (req, res) => {
     try {
         res.clearCookie(COOKIE_NAME, {
            httpOnly: false,
-          // domain: "localhost",
+           domain: "render-ai-chat-bot-backend.vercel.app",
            signed: true,
            path: "/",
            secure: true
