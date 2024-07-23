1784868e4af6ac7c1663fe9454d67668693422ef
Mujtaba Mubasher <81616586+MujtabaMubasher@users.noreply.github.com>
2024-07-23 14:03:12 +0500

    Update user-controller.js

--- backend/dist/controllers/user-controller.js
+++ backend/dist/controllers/user-controller.js
@@ -129,7 +129,7 @@ const verifyUser = async (req, res) => {
 };
 const logout = async (req, res) => {
     try {
-        options = {
+       const options = {
             httpOnly: true,
             domain: "render-ai-chat-bot-backend.vercel.app",
             signed: true,
